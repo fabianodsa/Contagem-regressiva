8c93c5a79982f02da57638af2d096c52d9e50a3a
fabianodsa <fabyanoduarte@hotmail.com>
2022-12-27 23:28:42 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const secondsContainer = document.querySelector('#seconds')
 const minutesContainer = document.querySelector('#minutes')
-const hourContainer = document.querySelector('#hour')
+const hourContainer = document.querySelector('#hours')
 const daysContainer = document.querySelector('#days')
 const nextYearContainer = document.querySelector('#year')
 const loading = document.querySelector('#loading')
 const contadorContainer = document.querySelector('#container')
 
-
 const proximoAno = new Date().getFullYear() + 1
 const dataInicial = new Date(`January 01 ${proximoAno} 00:00:00`)
 
 nextYearContainer.textContent = proximoAno
+
+const inserirNumero = unidade => unidade < 10 ? '0' + unidade : unidade
+
+const inserirContador = ({ days, hours, minutes, seconds }) => {
+    secondsContainer.textContent = inserirNumero(seconds)
+    minutesContainer.textContent = inserirNumero(minutes)
+    hourContainer.textContent = inserirNumero(hours)
+    daysContainer.textContent = inserirNumero(days)
+}
 
 const atualizaAno = () => {
     const anoAtual = new Date()
@@ -18,21 +26,16 @@ const atualizaAno = () => {
     const days = Math.floor(diferencaAno / 1000 / 60 / 60 / 24)
     const hours = Math.floor(diferencaAno / 1000 / 60 / 60) % 24
     const minutes = Math.floor(diferencaAno / 1000 / 60) % 60
-    const seconds = Math.floor(diferencaAno / 1000) % 60
+    const seconds = Math.floor(diferencaAno / 1000) % 60    
 
-    secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
-    minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
-    hourContainer.textContent = hours < 10 ? '0' + hours : hours
-    daysContainer.textContent = days < 10 ? '0' + days : days
-    
+    inserirContador({ days, hours, minutes, seconds })
 }
 
-setTimeout(() => {
+const removerLoading = () => {
     loading.remove()
     contadorContainer.style.display = 'flex'
-}, 1000)
+}
+
+setTimeout(removerLoading, 1000)
 
 setInterval(atualizaAno, 1000)
-
-
-
