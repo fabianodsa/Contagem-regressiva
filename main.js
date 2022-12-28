daebfc31fbb9f6a783f9b751944c9b8423d046de
fabianodsa <fabyanoduarte@hotmail.com>
2022-12-27 21:26:01 -0300

    Criando função para exibir os dias na tela

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const secondsContainer = document.querySelector('#seconds')
-
-// const days = document.querySelector('#days')
-// const hour = document.querySelector('#hour')
-// const minutes = document.querySelector('#minutes')
-
-
-
+const daysContainer = document.querySelector('#days')
+const hourContainer = document.querySelector('#hour')
+const minutesContainer = document.querySelector('#minutes')
 
 const proximoAno = new Date().getFullYear() + 1
 const dataInicial = new Date(`January 01 ${proximoAno} 00:00:00`)
@@ -19,33 +15,12 @@ const atualizAno = () => {
     const seconds = Math.floor(diferencaAno / 1000) % 60
 
     secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
+    daysContainer.textContent = days < 10 ? '0' + days : days
+    secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
+    secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
 }
 
 setInterval(atualizAno, 1000)
 
 
-
-
-
-
-
-
-// function startTimer(duration, display) {
-//     var timer = duration, minutes, seconds;
-//     setInterval(function () {
-//         minutes = parseInt(timer / 60, 10);
-//         seconds = parseInt(timer % 60, 10);
-//         minutes = minutes < 10 ? "0" + minutes : minutes;
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
-//         display.textContent = minutes + ":" + seconds;
-//         if (--timer < 0) {
-//             timer = duration;
-//         }
-//     }, 1000);
-// }
-// window.onload = function () {
-//     var duration = 60 * 5; // Converter para segundos
-//         display = document.querySelector('#timer'); // selecionando o timer
-//     startTimer(duration, display); // iniciando o timer
-// };
 
