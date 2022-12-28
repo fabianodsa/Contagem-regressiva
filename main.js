fdbc7f15433b53742204202e7c90f9d212db4a95
fabianodsa <fabyanoduarte@hotmail.com>
2022-12-28 10:54:43 -0300

    Atualizando o readme

--- main.js
+++ main.js
@@ -4,38 +4,38 @@ const hourContainer = document.querySelector('#hours')
 const daysContainer = document.querySelector('#days')
 const nextYearContainer = document.querySelector('#year')
 const loading = document.querySelector('#loading')
-const contadorContainer = document.querySelector('#container')
+const countdownContainer = document.querySelector('#container')
 
-const proximoAno = new Date().getFullYear() + 1
-const dataInicial = new Date(`January 01 ${proximoAno} 00:00:00`)
+const nextYear = new Date().getFullYear() + 1
+const currentTime = new Date(`January 01 ${nextYear} 00:00:00`)
 
-nextYearContainer.textContent = proximoAno
+nextYearContainer.textContent = nextYear
 
-const inserirNumero = unidade => unidade < 10 ? '0' + unidade : unidade
+const getUnit = unidade => unidade < 10 ? '0' + unidade : unidade
 
-const inserirContador = ({ days, hours, minutes, seconds }) => {
-    secondsContainer.textContent = inserirNumero(seconds)
-    minutesContainer.textContent = inserirNumero(minutes)
-    hourContainer.textContent = inserirNumero(hours)
-    daysContainer.textContent = inserirNumero(days)
+const countdown = ({ days, hours, minutes, seconds }) => {
+    secondsContainer.textContent = getUnit(seconds)
+    minutesContainer.textContent = getUnit(minutes)
+    hourContainer.textContent = getUnit(hours)
+    daysContainer.textContent = getUnit(days)
 }
 
-const atualizaAno = () => {
-    const anoAtual = new Date()
-    const diferencaAno = dataInicial - anoAtual
-    const days = Math.floor(diferencaAno / 1000 / 60 / 60 / 24)
-    const hours = Math.floor(diferencaAno / 1000 / 60 / 60) % 24
-    const minutes = Math.floor(diferencaAno / 1000 / 60) % 60
-    const seconds = Math.floor(diferencaAno / 1000) % 60    
+const uptadeYear = () => {
+    const thisYear = new Date()
+    const difference = currentTime - thisYear
+    const days = Math.floor(difference / 1000 / 60 / 60 / 24)
+    const hours = Math.floor(difference / 1000 / 60 / 60) % 24
+    const minutes = Math.floor(difference / 1000 / 60) % 60
+    const seconds = Math.floor(difference / 1000) % 60    
 
-    inserirContador({ days, hours, minutes, seconds })
+    countdown({ days, hours, minutes, seconds })
 }
 
 const removerLoading = () => {
     loading.remove()
-    contadorContainer.style.display = 'flex'
+    countdownContainer.style.display = 'flex'
 }
 
 setTimeout(removerLoading, 1000)
 
-setInterval(atualizaAno, 1000)
+setInterval(uptadeYear, 1000)
