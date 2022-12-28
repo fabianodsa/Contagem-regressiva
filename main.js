a06e9f03b366494e943e24227bc6aa668ddc81b4
fabianodsa <fabyanoduarte@hotmail.com>
2022-12-27 22:54:07 -0300

    Criando função para que após a atualização da tela, o spinner seja removido

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const secondsContainer = document.querySelector('#seconds')
-const daysContainer = document.querySelector('#days')
-const hourContainer = document.querySelector('#hour')
 const minutesContainer = document.querySelector('#minutes')
+const hourContainer = document.querySelector('#hour')
+const daysContainer = document.querySelector('#days')
+const nextYearContainer = document.querySelector('#year')
+const loading = document.querySelector('#loading')
+const contadorContainer = document.querySelector('#container')
+
 
 const proximoAno = new Date().getFullYear() + 1
 const dataInicial = new Date(`January 01 ${proximoAno} 00:00:00`)
 
-const atualizAno = () => {
+nextYearContainer.textContent = proximoAno
+
+const atualizaAno = () => {
     const anoAtual = new Date()
     const diferencaAno = dataInicial - anoAtual
     const days = Math.floor(diferencaAno / 1000 / 60 / 60 / 24)
@@ -15,12 +21,18 @@ const atualizAno = () => {
     const seconds = Math.floor(diferencaAno / 1000) % 60
 
     secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
-    daysContainer.textContent = days < 10 ? '0' + days : days
-    hourContainer.textContent = hours < 10 ? '0' + hours : hours
     minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
+    hourContainer.textContent = hours < 10 ? '0' + hours : hours
+    daysContainer.textContent = days < 10 ? '0' + days : days
+    
 }
 
-setInterval(atualizAno, 1000)
+setTimeout(() => {
+    loading.remove()
+    contadorContainer.style.display = 'flex'
+}, 1000)
+
+setInterval(atualizaAno, 1000)
 
 
 
